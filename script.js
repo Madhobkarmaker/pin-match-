3a06b7e87957e4a4195511f3c00fb3d00de21dcd
Madhobkarmaker <67518463+Madhobkarmaker@users.noreply.github.com>
2020-07-27 20:40:36 +0600

    pin matcher project done

--- script.js
+++ script.js
@@ -1,1 +1,42 @@
-console.log('hello brother');
+// random pin generate and display random pin
+
+function randomPinInput(min, max){
+        document.getElementById("display-pin").value = Math.floor(Math.random() * (max - min + 1) + min);
+        
+}
+
+//  Input digit in the screen-2
+
+function digitInput(value){
+    document.getElementById("screen").value += value;
+}
+
+// remove one by one number by clicking '<'
+
+document.getElementById("remove-digit").addEventListener('click',function(){
+    document.getElementById("screen").value = document.getElementById("screen").value.slice(0,-1);
+})
+
+// clear the screen-2 by clicking 'c'
+
+document.getElementById("clear-screen").addEventListener('click',function(){
+    document.getElementById("screen").value = " " ; 
+})
+
+// display message after clicking submit button
+
+document.getElementById("submit").addEventListener('click',function(){
+    if(document.getElementById("screen").value == document.getElementById("display-pin").value){
+        document.getElementById("alertMessage2").style.display = 'block' ;
+        document.getElementById("screen").value = " " ;
+        document.getElementById("display-pin").value = " " ;
+        
+    }
+    else{
+        document.getElementById("alertMessage1").style.display = 'block' ;
+        document.getElementById("screen").value = " " ;
+         
+    }
+    
+ })
+
