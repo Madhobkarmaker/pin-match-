cfe9f74bf544db875d09e5216af41faa95ef51ea
Madhobkarmaker <67518463+Madhobkarmaker@users.noreply.github.com>
2020-07-29 21:34:02 +0600

    pin matcher project complete

--- script.js
+++ script.js
@@ -1,51 +1,66 @@
-
 // random pin generate and display random pin
-
 function randomPinInput(min, max){
-        document.getElementById("display-pin").value = Math.floor(Math.random() * (max - min + 1) + min);
-        notDisplay();
-        
+    document.getElementById("display-pinScreen").value = Math.floor(Math.random() * (max - min + 1) + min);
+    document.getElementById("matchMessage").style.display = "none";
+    document.getElementById("misMatchMessage").style.display = "none";
+    
 }
 
-//  Input digit in the screen-2
-
+//  Input digit in the inputScreen
 function digitInput(value){
-    document.getElementById("screen").value += value;
+    document.getElementById("input-pinScreen").value += value;
 }
 
 // remove one by one number by clicking '<'
-
 document.getElementById("remove-digit").addEventListener('click',function(){
-    document.getElementById("screen").value = document.getElementById("screen").value.slice(0,-1);
+    document.getElementById("input-pinScreen").value = document.getElementById("input-pinScreen").value.slice(0,-1);
 })
 
 // clear the screen-2 by clicking 'c'
-
 document.getElementById("clear-screen").addEventListener('click',function(){
-    document.getElementById("screen").value = " " ; 
+    document.getElementById("input-pinScreen").value = " " ; 
 })
 
 // display message after clicking submit button
-
-document.getElementById("submit").addEventListener('click',function(){
-    if(document.getElementById("screen").value == document.getElementById("display-pin").value){
-        document.getElementById("alertMessage2").style.display = 'block' ;
-        document.getElementById("screen").value = " " ;
-        document.getElementById("display-pin").value = " " ;
+function submitButton() {
+    var generatePin = document.getElementById("display-pinScreen").value;
+    var inputPin = document.getElementById("input-pinScreen").value;
+    if (inputPin == generatePin) {
+        document.getElementById("matchMessage").style.display = "block";
+        document.getElementById("misMatchMessage").style.display = "none";
+        document.getElementById("try-action").style.display = "none";
+        document.getElementById("display-pinScreen").value = "" ;
+        document.getElementById("input-pinScreen").value = "" ;
         
+    } 
+    else {
+        document.getElementById("matchMessage").style.display = "none";
+        document.getElementById("misMatchMessage").style.display = "block";
+        document.getElementById("input-pinScreen").value = "" ;
+        tryLeftFunction();
     }
-    else{
-        document.getElementById("alertMessage1").style.display = 'block' ;
-        document.getElementById("screen").value = " " ;
-         
-    }
- })
-
- // not display the bottom message
-
-function notDisplay(){
-    document.getElementById("alertMessage1").style.display = 'none';
-    document.getElementById("alertMessage2").style.display = 'none';
 }
 
-notDisplay();
+// tryLeft action 
+// if submit wrong input three times then submit button will remove 
+
+let tryCount = 0;
+function tryLeftFunction() {
+    var generatePin = document.getElementById("display-pinScreen").value;
+    var inputPin = document.getElementById("input-pinScreen").value;
+    tryCount++ ;
+    if (tryCount == 1 && inputPin !== generatePin) {
+        document.getElementById("try-action").innerText = "2 try left";
+    }
+    else if (tryCount == 2 && inputPin !== generatePin) {
+        document.getElementById("try-action").innerText = "1 try left";
+    }
+    else if (tryCount == 3 && inputPin !== generatePin) {
+        document.getElementById("try-action").style.display = "none";
+        document.getElementById("submitBtn").style.display = "none";
+    }
+}
+
+// hide the match & mismatch notifications
+document.getElementById("matchMessage").style.display = "none"
+document.getElementById("misMatchMessage").style.display = "none"
